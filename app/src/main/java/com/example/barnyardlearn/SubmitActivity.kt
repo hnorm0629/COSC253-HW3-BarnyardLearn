5207e4c46bd840b947bbb2ab41086dcc57e21c68
Hannah Norman <hen9@georgetown.edu>
2022-02-09 16:25:23 -0500

    BIG commit #2. Added comments among other things... - Updated aspects of quiz UI - Click volume button to hear noise - Noise stops when activity exited - Added two more questions to quiz - Combined animal activities into a single file - etc.

--- app/src/main/java/com/example/barnyardlearn/SubmitActivity.kt
+++ app/src/main/java/com/example/barnyardlearn/SubmitActivity.kt
@@ -4,15 +4,31 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
+
+/*
+Hannah Norman
+Sophia Petersen
+COSC-253 HW3
+02/09/2022
+ */
 
 class SubmitActivity : AppCompatActivity() {
+    private lateinit var score: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_submit)
 
+        // retrieve and update score TextView
+        score = findViewById(R.id.score)
+        val scoreAmt = intent.getStringExtra("Score").toString()
+        score.text = scoreAmt
+
         supportActionBar!!.elevation = 0f   // remove action bar drop shadow
     } // onCreate
 
+    // move to Main Menu activity
     fun mainMenu(view: View) {
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
